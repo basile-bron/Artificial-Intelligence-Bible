1e034381cbfb4e8963d0a2224ada6db6667bb48c
Basile Bron <basile.bron@protonmail.com>
2020-01-16 14:45:44 +0000

    dbscan playground, possible to use wine dataset

--- unsupervised_learning/DBSCAN.py
+++ unsupervised_learning/DBSCAN.py
@@ -4,59 +4,53 @@ from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 import numpy as np
+import pandas as pd
 
 """
 This is a playground for the DBSCAN algorithm
-Iwould try epsilon 0.13 and min_sample 5    
+Try epsilon 0.13 and min_sample 5
 """
-
-
-"""ploting result """
-def display_graph(X, y):
-    # scatter plot, dots colored by class value
-    df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
-    colors = {0:'red', 1:'blue'}
-    fig, ax = plt.subplots()
-    grouped = df.groupby('label')
-    for key, group in grouped:
-        group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
-    plt.show()
 
 #generate dataset
 X, y = make_moons(n_samples=500, noise=0.1)
 
-#find a good epsilon using distance graph
-neigh = NearestNeighbors(n_neighbors=2)
-nbrs = neigh.fit(X)
-distances, indices = nbrs.kneighbors(X)
+#uncomment to try with wine dataset
+#X = pd.read_csv("../data/winequality-red.csv")
 
-distances = np.sort(distances, axis=0)
-distances = distances[:,1]
-plt.title('distances')
-plt.plot(distances)
-plt.show()
+def distance_graph():
+    #find a good epsilon using distance graph
+    neigh = NearestNeighbors(n_neighbors=2)
+    nbrs = neigh.fit(X)
+    distances, indices = nbrs.kneighbors(X)
+
+    distances = np.sort(distances, axis=0)
+    distances = distances[:,1]
+    plt.title('distances')
+    plt.plot(distances)
+    plt.show()
+
+distance_graph()
+def run_DBSCAN(eps, min_samples, X):
+    #distance_graph()
+    eps = input("epsilon  :\n")
+    min_samples = input("min_samples  :\n")
+
+    #define epsilon neighbourhood and minimum sample in the neighbourhood based on the result
+    dbscan = DBSCAN(eps=float(eps), min_samples=float(min_samples))
+    dbscan.fit(X)
+    #Here are the labels
+    clusters = dbscan.labels_
 
 
-eps = input("epsilon  :\n")
-min_samples = input("min_samples  :\n")
+    colors = ['olive', 'maroon','royalblue',  'forestgreen', 'mediumorchid', 'tan', 'deeppink',  'goldenrod', 'lightcyan', 'navy']
+    #colors = { 0:'green', 1:'yellow'}
+    vectorizer = np.vectorize(lambda x: colors[x % len(colors)])
 
-#define epsilon neighbourhood and minimum sample in the neighbourhood based on the result
-dbscan = DBSCAN(eps=float(eps), min_samples=float(min_samples))
-dbscan.fit(X)
-#Here are the labels
-clusters = dbscan.labels_
-#np.unique(clusters)
-#dbscan.core_sample_indices_.shape
-#dbscan.components_.shape
+    try:
+        X = pd.DataFrame.to_numpy(X)
+    except:
+        print("")
+    plt.scatter(X[:, 0], X[:, 1], c=vectorizer(clusters), s=50, cmap='viridis')
+    plt.show()
 
-
-colors = ['olive', 'maroon','royalblue',  'forestgreen', 'mediumorchid', 'tan', 'deeppink',  'goldenrod', 'lightcyan', 'navy']
-#colors = { 0:'green', 1:'yellow'}
-vectorizer = np.vectorize(lambda x: colors[x % len(colors)])
-
-plt.title('input data')
-plt.scatter(X[:,0], X[:,1])
-plt.show()
-plt.title('output data')
-plt.scatter(X[:,0], X[:,1], c=vectorizer(clusters))
-plt.show()
+run_DBSCAN(0.13, 5, X)
--- unsupervised_learning/K-Mean.py
+++ unsupervised_learning/K-Mean.py
@@ -7,9 +7,9 @@ from sklearn.cluster import KMeans
 def k_mean(nbr_cluster,data):
     #define the number of cluster
     k = nbr_cluster
-    kmeans = KMeans(n_clusters=k)
-    y_kmeans = kmeans.fit_predict(data)
-    return y_kmeans
+    kmean_result = KMeans(n_clusters=k)
+    y_kmeans = kmean_result.fit_predict(data)
+    return y_kmeans, kmean_result
     #print(X)
     #kmeans.cluster_centers_
 
@@ -21,18 +21,19 @@ def k_mean(nbr_cluster,data):
 #X_new = np.array([0,2])
 #kmens.predict(X_new)
 
-def plot(y_kmeans, data):
+def plot(y_kmeans, data, kmean_result):
     data = pd.DataFrame.to_numpy(data)
     plt.scatter(data[:, 0], data[:, 1], c=y_kmeans, s=50, cmap='viridis')
-    centers = kmeans.cluster_centers_
+    centers = kmean_result.cluster_centers_
     plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-
+    plt.show()
 #source https://jakevdp.github.io/PythonDataScienceHandbook/05.11-k-means.html
 def main(nbr_cluster, path):
     data = pd.read_csv(path)
-    y_kmeans = k_mean(nbr_cluster,data)
-    plot(y_kmeans,data)
+    y_kmeans, kmean_result = k_mean(nbr_cluster,data)
+    plot(y_kmeans,data, kmean_result)
+    
 
 
-main(31, "data/winequality-red.csv")
-main(7, "data/winequality-white.csv")
+main(31, "../data/winequality-red.csv")
+main(7, "../data/winequality-white.csv")
