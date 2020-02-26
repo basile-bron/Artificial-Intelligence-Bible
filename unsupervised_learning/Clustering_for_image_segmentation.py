f823379bf51caa696e034c48b9597a4c52a9002f
BRON Basile <Basile.bron@protonmail.com>
2020-02-26 16:49:50 +0000

    clustering kmean final

--- unsupervised_learning/Clustering_for_image_segmentation.py
+++ unsupervised_learning/Clustering_for_image_segmentation.py
@@ -1,9 +1,38 @@
 from matplotlib.image import imread
 from sklearn.cluster import KMeans
 import os
-image = imread(os.path.join("../data/landscape.jpg"))
+import cv2
+import matplotlib.pyplot as plt
+import numpy as numpy
+
+image_rgb = imread(os.path.join("C:/Users/Skull/Documents/github/Artificial-Intelligence-Lexical/data/landscape.jpg"))
 print(image.shape)
-X = image.reshape(-1, 3)
-kmeans = KMeans(n_clusters=8).fit(X)
+#convert to HSV (apparently it is better than RGB for detection )
+image_hsv=cv2.cvtColor(image_rgb,cv2.COLOR_BGR2RGB)
+#converts the MxNx3 image into a Kx3 matrix where K=MxN and each row is now a vector in the 3-D space of RGB.
+X = image_hsv.reshape(-1, 3)
+
+#Kmean
+kmeans = KMeans(n_clusters=3).fit(X)
 segmented_img = kmeans.cluster_centers_[kmeans.labels_]
 segmented_img = segmented_img = segmented_img.reshape(image.shape)
+segmented_img.shape
+#Print the image
+# Display all results, alongside original image
+plt.figure(1)
+plt.clf()
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(image_rgb)
+
+plt.figure(2)
+plt.clf()
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(image_hsv)
+
+plt.figure(3)
+plt.clf()
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(segmented_img.astype('uint8'))
