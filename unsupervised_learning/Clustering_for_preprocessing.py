b984c4ea99f70005ef63ee1534ab9953a9e45e95
Basile Bron <basile.bron@protonmail.com>
2020-02-28 13:29:35 +0000

    clustering preprocessing prefinal

--- unsupervised_learning/Clustering_for_preprocessing.py
+++ unsupervised_learning/Clustering_for_preprocessing.py
@@ -12,24 +12,40 @@ log_reg.fit(X_train, y_train)
 
 #evaluate accuracy
 log_reg.score(X_test, y_test)
-###########################
-
+###############################################
 from sklearn.pipeline import Pipeline
 
 pipeline = Pipeline([
-    ("kmeans", KMeans(n_clusters=50)),
-    ("log_reg", LogisticRegression(solver='lbfgs', multi_class='auto')),
+    ("kmeans", KMeans(n_clusters=90)),
+    ("log_reg", LogisticRegression(solver='lbfgs', multi_class='auto',max_iter=10000)),
 ])
 
 pipeline.fit(X_train, y_train)
 
 pipeline.score(X_test, y_test)
-################
+###################################################
 from sklearn.model_selection import GridSearchCV
 
 param_grid = dict(kmeans__n_clusters=range(2,100))
-grid_clf = GridSearchCV(pipeline, param_grid, cv=3, verbose=2)
+grid_clf = GridSearchCV(pipeline, {
+    'alpha': [0.001, 0.0001], 'average': [True, False],
+    'shuffle': [True, False], 'max_iter': [5], 'tol': [None]
+}, param_grid, cv=3, verbose=1)
 grid_clf.fit(X_train, y_train)
 
 grid_clf.best_params_
 grid_clf.score(X_test, y_test)
+
+# CLUSTERING FOR SEMI SUPERVISED LEARNING
+n_labeled = 50
+log_reg = LogisticRegression()
+log_reg.fit(X_train[:n_labeled], y_train[:labeled])
+#performance of the model on the test set ?
+log_reg.score(X_test, y_test)
+
+##########################################
+k = 50
+kmeans = KMeans(n_clusters=k)
+X_digits_dist = kmeans.fit_transform(X_train)
+representative_digit_idx = np.argmin(X_digits_dist, axis=0)
+X_representative_digits = X_train[representative_digit_idx]
