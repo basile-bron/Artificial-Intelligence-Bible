0ef3b49ee9224d0974575bc5b2194ee664ec0e11
BRON Basile <Basile.bron@protonmail.com>
2020-03-02 11:24:23 +0000

    oupsi doupsi

--- unsupervised_learning/config.py
+++
@@ -1,8 +0,0 @@
-config= {
-    # Google Cloud Credentials
-    'CLIENT_ID': '21470454673-1e9gom8mg42s8ufpbee6ah206csgn9s6.apps.googleusercontent.com',
-    'CLIENT_SECRET': '2mm-R6qvlIQWBYTEooN86XH8',
-    'OAUTH_SCOPE': ['https://www.googleapis.com/auth/webmasters.readonly','https://www.googleapis.com/auth/analytics.readonly'],
-    'GOOGLE_CREDENTIALS': 'XXXXXXXXXXXXXXXXXXXXXX',
-
-}
